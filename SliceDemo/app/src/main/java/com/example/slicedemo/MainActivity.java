a50df2fd29c01ca66c45dfb736f31588ba64aa90
urvish.rana <urvish.rana@volansystech.com>
2019-02-26 11:09:52 +0530

    - updated bug fix
    Signed-off-by: urvish.rana <urvish.rana@volansystech.com>

--- SliceDemo/app/src/main/java/com/example/slicedemo/MainActivity.java
+++ SliceDemo/app/src/main/java/com/example/slicedemo/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.slicedemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     public static int mCount=0;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView=findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
+        if (savedInstanceState != null)
+            textView.setText(Integer.toString(savedInstanceState.getInt("temp")));
+        else textView.setText(Integer.toString(mCount));
         ((Button)findViewById(R.id.plus)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,5 +42,11 @@ public class MainActivity extends AppCompatActivity {
             Uri uri=Uri.parse("content://com.example.slicedemo/count");
             context.getContentResolver().notifyChange(uri,null);
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt("temp", mCount);
     }
 }
